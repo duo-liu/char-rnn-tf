8eb47527f799b4ffc8962542e16d62fbdae7c77b
duo-liu <duo.liu@kikatech.com>
2017-12-16 17:46:06 +0800

    修改了compare_prob.py

--- compare_prob.py
+++ compare_prob.py
@@ -4,6 +4,8 @@ import _pickle
 import Config
 import Model
 import time
+import math
+from collections import OrderedDict
 
 config_tf = tf.ConfigProto()
 config_tf.gpu_options.allow_growth = True
@@ -13,6 +15,7 @@ config_tf.intra_op_parallelism_threads = 1
 config = Config.Config()
 
 char_to_idx, idx_to_char = _pickle.load(open(config.model_dir + '/vocab.bin', 'rb'))
+frequency = _pickle.load(open('/home/liuduo/frequency.bin', 'rb'))
 
 config.vocab_size = len(char_to_idx)
 
@@ -42,34 +45,38 @@ def main(_):
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' ' + 'Done!')
 
         # dictation、correction、replacement、nonsense
-        test_pairs = [('How about we have a dinner tomorrow night?', 'and then see a movie'),
-                      ('How about we have a dinner tomorrow night?', 'lunch'),
-                      ('How about we have a dinner tomorrow night?', 'tomorrow afternoon'),
-                      ('How about we have a dinner tomorrow night?', 'What is your name'),
-                      ('I look forward to working with you in the following months.', 'It is so great'),
-                      ('I look forward to working with you in the following months.', 'weeks'),
-                      ('I look forward to working with you in the following months.', 'your team'),
-                      ('I look forward to working with you in the following months.', 'I am hungry'),
-                      ("What's the weather tomorrow?", 'How about go to the park'),
-                      ("What's the weather tomorrow?", 'the day after tomorrow'),
-                      ("What's the weather tomorrow?", 'today'),
-                      ("What's the weather tomorrow?", 'What is your name')]
+        test_pairs = [('How about we have a dinner tomorrow night? ', 'and then see a movie'),
+                      ('How about we have a dinner tomorrow night? ', 'lunch'),
+                      ('How about we have a dinner tomorrow night? ', 'tomorrow afternoon'),
+                      ('How about we have a dinner tomorrow night? ', 'What is your name'),
+                      ('I look forward to working with you in the following months. ', 'It is so great'),
+                      ('I look forward to working with you in the following months. ', 'weeks'),
+                      ('I look forward to working with you in the following months. ', 'your team'),
+                      ('I look forward to working with you in the following months. ', 'I am hungry'),
+                      ("What's the weather tomorrow? ", 'what about go to the park'),
+                      ("What's the weather tomorrow? ", 'the day after tomorrow'),
+                      ("What's the weather tomorrow? ", 'today'),
+                      ("What's the weather tomorrow? ", 'What is your name')]
+        output_lines = list()
         for s1, s2 in test_pairs:
             p_s2_s1 = get_prob(s1, s2, mtest, session)
             p_s2 = get_prob2(s2, mtest, session)
-            result = p_s2_s1 - p_s2
-            print(s1 + '    ' + s2 + '    ' + str(p_s2_s1) + '    ' + str(p_s2) + '    ' + str(result))
+            # result = p_s2_s1 - p_s2
+            print(s1 + '    ' + s2 + '    ' + str(p_s2_s1))
+            output_lines.append(s1 + '    ' + s2 + '    ' + str(p_s2_s1) + '\n')
+        open('/Users/liuduo/Desktop/result.txt', 'w').writelines(output_lines)
 
 
 def get_prob2(s1, mtest, session):
     s1_chars = list(s1.lower())
 
-    start_idx = char_to_idx[' ']
+    start_idx = char_to_idx[s1_chars[0]]
     _state = mtest.initial_state.eval()
     prob, _state = run_epoch(session, mtest, np.int32([start_idx]), tf.no_op(), _state)
     s1_probs = list()
-    for char in s1_chars:
-        idx = char_to_idx[char]
+    s1_probs.append(frequency[s1_chars[0]])
+    for i in range(1, len(s1_chars)):
+        idx = char_to_idx[s1_chars[i]]
         s1_probs.append(prob.reshape(-1)[idx])
         prob, _state = run_epoch(session, mtest, np.int32([idx]), tf.no_op(), _state)
     return sum([np.log(1e-20 + x) for x in s1_probs])
@@ -92,11 +99,34 @@ def get_prob(s1, s2, mtest, session):
         beams = [(beams[0][0], beams[0][1] + [char], char_index, _state)]
     # 依次将s2的所有字符顺序输入到RNN模型中，计算每个字符产生的概率，从而得到最终整个s2字符序列的产生概率
     s2_probs = list()
-    for char in s2_chars:
+    for i, char in enumerate(s2_chars):
+        if i == 0:
+            top_indices = np.argsort(prob.reshape(-1) * -1)[:5]
+            first_letter_probs = OrderedDict([(idx_to_char[x], prob.reshape(-1)[x]) for x in top_indices])
         idx = char_to_idx[char]
         s2_probs.append(prob.reshape(-1)[idx])
         prob, _state = run_epoch(session, mtest, np.int32([idx]), tf.no_op(), _state)
-    return sum([np.log(1e-20 + x) for x in s2_probs])
+
+    # 1. 相乘然后开s2.length次方
+    result = 1
+    for x in s2_probs:
+        result *= x
+    return math.pow(result, 1 / len(s2_chars)), first_letter_probs, s2_probs
+
+    # 2. 相乘之后得到P(s2|s1)，然后再除以get_prob2()方法得到的P(s2)
+    # return sum([np.log(1e-20 + x) for x in s2_probs])
+
+    # 3. 直接相加
+    # return sum(s2_probs)
+
+    # 4. 套用OpenNMT的Length normalization
+    # p_s2s1 = 1
+    # for x in s2_probs:
+    #     p_s2s1 *= x
+    # numerator = np.log(p_s2s1)
+    # alpha = 1.0
+    # denominator = np.power(5 + len(s2_chars), alpha) / np.power(6, alpha)
+    # return numerator / denominator
 
 
 if __name__ == "__main__":
