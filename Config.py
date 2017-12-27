97d18e0b4bfbe763c9d4c132b4f27d642f64fab2
duo-liu <duo.liu@kikatech.com>
2017-12-27 19:50:53 +0800

    修改了模型的配置参数

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 class Config(object):
     init_scale = 0.04
     learning_rate = 0.001
-    max_grad_norm = 15
+    max_grad_norm = 5
     num_layers = 3
-    num_steps = 80  # number of steps to unroll the RNN for
+    num_steps = 100  # number of steps to unroll the RNN for
     hidden_size = 1024  # size of hidden layer of neurons
     iteration = 30
-    keep_prob = 0.5
-    batch_size = 128
+    keep_prob = 0.75
+    batch_size = 64
     ckpt_steps = 2000
     model_dir = '/home/liuduo/ckpt'
     model_path = model_dir + '/model.ckpt'  # the path of model that need to save or load
